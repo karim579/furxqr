a7c71b3a172233a4b693422982c77c175bfceb59
karim579 <karimdalam123@gmail.com>
2024-10-16 17:33:31 +0300

    add initial files

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', function(event) {
+    console.log('Service worker installing...');
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+    console.log('Service worker activating...');
+});
+
+self.addEventListener('fetch', function(event) {
+    console.log('Fetching:', event.request.url);
+});
